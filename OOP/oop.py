4b215020972e231ea8e6e7f980c45b9814b923d8
SabrinaBmz <78277643+SabrinaBmz@users.noreply.github.com>
2021-11-01 15:00:46 +0100

    6th commit

---
+++ OOP/oop.py
@@ -0,0 +1,17 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+    def are(self):
+        return self.length * self.width
+
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Vehicle:
+    pass
+
+class Bus(Vehicle):
+    pass
